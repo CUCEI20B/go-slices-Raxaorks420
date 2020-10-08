c60cccf1f200106324bf68ebb81183336c249483
Andrés Ornelas <jose.ornelasa@alumnos.udg.mx>
2020-10-08 18:49:17 -0500

    First commit

--- main.go
+++ main.go
@@ -2,6 +2,24 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+
+	var n int
+	fmt.Scan(&n)
+
+	s := make([]int, n+1)
+	s[0] = n
+
+	var input int
+	for i := 0; i < n; i++ {
+		fmt.Scan(&input)
+		s[i+1] = input
+	}
+
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+
+	fmt.Println(sum)
 }
